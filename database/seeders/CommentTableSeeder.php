f049a0a4a647f48c190583ddf2b45beb7b3aee6e
Josh Codd <jjc21@live.co.uk>
2021-10-28 20:23:00 +0100

    Seed comment data.

---
+++ database/seeders/CommentTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Comment;
+use Illuminate\Database\Seeder;
+
+class CommentTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $comment = new Comment();
+        $comment->user_id = 1;
+        $comment->post_id = 1;
+        $comment->content = 'This is my first comment.';
+        $comment->save();
+
+        $comments = Comment::factory()->count(10)->create();
+    }
+}
