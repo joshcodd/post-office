afc08a685751eb84eb795a9266ee8e0cac7cee9a
Josh Codd <jjc21@live.co.uk>
2021-11-02 15:29:17 +0000

    Use tailwind

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,8 +2,10 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
     <title>SPLIT - @yield('title')</title>
 </head>
 
