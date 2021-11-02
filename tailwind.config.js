2d1d86f20a5cc2bc96414fabd371d21ee4f7ef6e
Josh Codd <jjc21@live.co.uk>
2021-11-02 17:30:14 +0000

    Add fonts.

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,8 @@ module.exports = {
         extend: {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                garamond: ['"EB Garamond"', 'serif'],
+                nunito: ['Nunito', 'sans-serif'],
             },
         },
     },
