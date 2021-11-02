709cc0985225242746f452ad0c635c34be55026b
Josh Codd <jjc21@live.co.uk>
2021-11-02 14:34:13 +0000

    Begin basic gui

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('title', 'Posts')
+
+@section('content')
+
+<p> Posts: </p>
+
+<ul>
+
+    @foreach ($posts as $post)
+        <li>
+            {{$post->title}}
+        </li>
+    @endforeach
+
+</ul>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,11 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.app');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('posts', [PostController::class, 'index']);
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
