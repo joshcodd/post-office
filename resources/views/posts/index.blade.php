3dca342a1a8821a938046e6188f7be25e5c3ce72
Josh Codd <jjc21@live.co.uk>
2021-11-04 09:50:58 +0000

    Add comment count.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -23,20 +23,26 @@
                     </span>
 
                     <div id={{ 'comment_btn_' . $post->id }} onclick="handleCommentBtnClick(this.id)"
-                        class="group float-right mt-0.5 mr-2 mb-2">
-                        <div id={{ 'comment_btn_clear_' . $post->id }} class=" invisible  group-hover:visible absolute">
+                        class="group float-right mt-0.5 mb-2">
+                        <div id={{ 'comment_btn_clear_' . $post->id }}
+                            class=" invisible  group-hover:visible absolute inline-block">
                             <svg xmlns=" http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24">
                                 <path
                                     d="M0 1v16.981h4v5.019l7-5.019h13v-16.981h-24zm13 12h-8v-1h8v1zm6-3h-14v-1h14v1zm0-3h-14v-1h14v1z" />
                             </svg>
                         </div>
 
-                        <div id={{ 'comment_btn_fill_' . $post->id }} class="visible  group-hover:invisible ">
+                        <div id={{ 'comment_btn_fill_' . $post->id }} class="visible  group-hover:invisible inline-block">
                             <svg xmlns=" http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24">
                                 <path
                                     d="M22 3v13h-11.643l-4.357 3.105v-3.105h-4v-13h20zm2-2h-24v16.981h4v5.019l7-5.019h13v-16.981zm-5 6h-14v-1h14v1zm0 2h-14v1h14v-1zm-6 3h-8v1h8v-1z" />
                             </svg>
+
                         </div>
+                        <div class="inline-block float-right ml-2 ">
+                            {{ $post->comments->count() }}
+                        </div>
+
                     </div>
                 </div>
 
