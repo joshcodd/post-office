fbeeb77760eaec96091d31d95ddd5377c9660fcf
Josh Codd <jjc21@live.co.uk>
2021-11-11 13:45:13 +0000

    Account for first name and surname instead of just name for auth.

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,7 +30,8 @@ class User extends Authenticatable
      * @var string[]
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'surname',
         'email',
         'password',
     ];
