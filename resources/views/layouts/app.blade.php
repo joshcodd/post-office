e1f1880c711848b61b660624e169833aeeb6d21b
Josh Codd <jjc21@live.co.uk>
2021-11-12 18:10:19 +0000

    Add error messages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,11 +19,18 @@
     @include('layouts.navigation')
 
     @if (session('message'))
-        <div class="p-2 block text-center bg-gray-100 z-50">
+        <div class="border-b border-gray-400 p-2 block text-center bg-gray-100 z-50">
             <p class="inline text-center"> {{ session('message') }}</p>
             <button class=" relative right-5 float-right inline" onclick="hideParent(this)">x</button>
         </div>
     @endif
+
+    @foreach ($errors->all() as $error)
+        <div class="border-b border-gray-400 p-2 block text-center bg-gray-100 z-50">
+            <p class="inline text-center text-spotify"> {{ $error }}</p>
+            <button class=" relative right-5 float-right inline" onclick="hideParent(this)">x</button>
+        </div>
+    @endforeach
 
     <div class="min-h-screen" id="root">
         <div>
