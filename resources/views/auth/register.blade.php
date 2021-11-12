da8a4ab2191caeee20ec0b3ab5114497cfa385f6
Josh Codd <jjc21@live.co.uk>
2021-11-12 12:25:02 +0000

    Style register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,59 +1,116 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <script src="{{ asset('js/global_functions.js') }}" defer></script>
+    <script src="{{ asset('js/log_in_script.js') }}" defer></script>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <title> PostOffice</title>
+</head>
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
+<body class="font-sans antialiased ">
 
-            <!-- Name -->
-            <div>
-                <x-label for="name" :value="__('Name')" />
+    <div class="sm:flex h-screen flex-row-reverse min-w-full justify-center items-center flex-grow ">
 
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
-            </div>
+        <div class=" fixed top-0 right-0 px-6 py-4 sm:block z-20">
+            <a href="{{ route('login') }}"
+                class=" 
+                                    border     md:ml-0 sm:inline rounded px-3 py-1.5 text-sm font-semibold text-black border-black  hover:bg-gray-700 hover:text-white z-20
+                                    ">Already
+                have an account?</a>
+        </div>
 
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
+        <div class="h-full flex justify-center items-center pt-6 sm:pt-0 bg-gray-50  sm:w-1/2 z-10">
+            <div class=" w-3/4 md:w-1/2">
+                <div class="text-5xl text-center">
+                    🏣
+                </div>
+                <div class="text-3xl text-center mt-4">
+                    Sign Up
+                </div>
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
+                <div class="mt-6 px-6 pt-4 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg w-max-20">
+                    <!-- Session Status -->
+                    <x-auth-session-status class="mb-4" :status="session('status')" />
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+                    <!-- Validation Errors -->
+                    <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
-            </div>
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
 
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
+                        <!-- First Name -->
+                        <div>
+                            <x-label for="first_name" :value="__('First Name')" />
 
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
+                            <x-input id="first_name" class="block mt-1 w-full" type="text" name="first_name"
+                                :value="old('first_name')" required autofocus />
+                        </div>
+
+                        <!-- Surname -->
+                        <div class="mt-4">
+                            <x-label for="surname" :value="__('Surname')" />
+
+                            <x-input id="surname" class="block mt-1 w-full" type="text" name="surname"
+                                :value="old('surname')" required autofocus />
+                        </div>
+
+                        <!-- Email Address -->
+                        <div class="mt-4">
+                            <x-label for="email" :value="__('Email')" />
+
+                            <x-input id="email" class="block mt-1 w-full" type="email" name="email"
+                                :value="old('email')" required />
+                        </div>
+
+                        <!-- Password -->
+                        <div class="mt-4">
+                            <x-label for="password" :value="__('Password')" />
+
+                            <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                                autocomplete="new-password" />
+                        </div>
+
+                        <!-- Confirm Password -->
+                        <div class="mt-4">
+                            <x-label for="password_confirmation" :value="__('Confirm Password')" />
+
+                            <x-input id="password_confirmation" class="block mt-1 w-full" type="password"
                                 name="password_confirmation" required />
-            </div>
+                        </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-button class="ml-4">
-                    {{ __('Register') }}
-                </x-button>
+                        <div class="flex items-center justify-end mt-4">
+                            <x-button class="ml-4">
+                                {{ __('Sign me up') }}
+                            </x-button>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+        </div>
+
+        <div class="relative text-center h-full flex justify-center items-center  pt-0 bg-gray-100  sm:w-1/2 ">
+            <img class="w-full h-full  object-cover " src={{ asset('images/home-bg.jpg') }} alt="">
+            <div class=" absolute text-center font-bold text-gray-100 ">
+                <div id="home_text_1" class="text-6xl p-4">
+                    POSTOFFICE
+                </div>
+                <div id="home_text_2" class="text-7xl p-4">
+                    Yūbinkyoku
+                </div>
+                <div id="home_text_3" class="text-8xl p-4">
+                    郵便局
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
