a3243b96c5b2cbfd132486579f9f913a2b8414be
Josh Codd <jjc21@live.co.uk>
2021-11-15 16:56:44 +0000

    Add validation to comments api

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -5,11 +5,19 @@ namespace App\Http\Controllers;
 use App\Models\Comment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class CommentController extends Controller
 {
     public function apiUpdate(Request $request, $id)
     {
+        $validator = Validator::make([$request['content']], ['required']);
+        if ($validator->fails()) {
+            return response()->json([
+                'messages' => $validator->errors,
+            ], 400);
+        }
+
         $comment = Comment::findOrFail($id);
         $comment->content = $request['content'];
         $comment->save();
@@ -19,6 +27,13 @@ class CommentController extends Controller
 
     public function apiStore(Request $request)
     {
+        $validator = Validator::make([$request['content']], ['required']);
+        if ($validator->fails()) {
+            return response()->json([
+                'messages' => $validator->errors,
+            ], 400);
+        }
+
         $comment = new Comment();
         $comment->post_id = $request['post_id'];
         $comment->user_id = Auth::User()->id;
