2e8500bc1ad9e9fed5ac16912d4227be05598359
Josh Codd <jjc21@live.co.uk>
2021-11-16 03:28:27 +0000

    integrate vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,20 @@
-require('./bootstrap');
+require("./bootstrap");
 
-require('alpinejs');
+require("alpinejs");
+
+window.Vue = require("vue").default;
+
+// Components
+Vue.component(
+    "comment-button",
+    require("./components/CommentButton.vue").default
+);
+
+Vue.component(
+    "comment-section",
+    require("./components/CommentSection.vue").default
+);
+
+const app = new Vue({
+    el: "#app",
+});
