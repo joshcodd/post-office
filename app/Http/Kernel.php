f25e78e030ec02bc01b1d0576c9d52d5b7ce3103
Josh Codd <jjc21@live.co.uk>
2021-11-18 12:17:46 +0000

    Change middleware name

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,6 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'item.owner' => \App\Http\Middleware\CheckOwnsItem::class,
+        'write.access' => \App\Http\Middleware\Authorize::class,
     ];
 }
--- routes/api.php
+++ routes/api.php
@@ -24,8 +24,8 @@ Route::middleware('auth:sanctum')->get('comments', [CommentController::class, 'a
 
 Route::middleware('auth:sanctum')->post('comments', [CommentController::class, 'apiStore'])->name('api.comments.store');
 
-Route::middleware('auth:sanctum', 'item.owner')->put('comments/{comment}', [CommentController::class, 'apiUpdate'])->name('api.comments.update');
+Route::middleware('auth:sanctum', 'write.access')->put('comments/{comment}', [CommentController::class, 'apiUpdate'])->name('api.comments.update');
 
-Route::middleware('auth:sanctum', 'item.owner')->delete('comments/{comment}', [CommentController::class, 'apiDestroy'])->name('api.comments.destroy');
+Route::middleware('auth:sanctum', 'write.access')->delete('comments/{comment}', [CommentController::class, 'apiDestroy'])->name('api.comments.destroy');
 
 Route::middleware('auth:sanctum')->post('users/clear-notifications', [UserController::class, 'clearNotifications'])->name('api.users.clear-notifictions');
