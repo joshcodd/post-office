cb472b92518c23a24f77aa326fb9a1135a14ce23
Josh Codd <jjc21@live.co.uk>
2021-11-25 16:42:10 +0000

    Add header image model

---
+++ database/migrations/2021_11_25_123558_create_headers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHeadersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('headers', function (Blueprint $table) {
+            $table->id();
+            $table->string('image_path');
+            $table->bigInteger('user_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('headers');
+    }
+}
