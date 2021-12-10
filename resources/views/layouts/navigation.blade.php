c920007d99c422c9ed8b971ad4a7bac13fdec905
Josh Codd <jjc21@live.co.uk>
2021-12-10 00:12:49 +0000

    Fix bold nav link

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,16 +15,15 @@
                     </a>
 
                     @if (Auth::User()->user_role == 'user')
-                        <button class="hoverSplitContainer font-nunito m-50-important" onclick="notificationsClick(false)">
+                        <button class="hoverSplitContainer font-nunito m-50-important font-light"
+                            onclick="notificationsClick(false)">
                             @if (Auth::User()->unreadNotifications()->count() > 0)
                                 <div name="notification_count"
                                     class="w-5 h-5 absolute circle bg-spotify pt-0.5 text-xs font-bold ">
                                     {{ Auth::User()->unreadNotifications()->count() }}
                                 </div>
                             @endif
-                            <div class="topHalf">
-                                Notifications
-                            </div>
+                            <div class="topHalf ">Notifications</div>
                             <div class="bottomHalf">Notifications</div>
                         </button>
 
