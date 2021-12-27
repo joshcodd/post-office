f5cf7da657134d58663227653794ee3e4d34e520
Josh Codd <jjc21@live.co.uk>
2021-12-27 20:19:16 +0000

    Use pusher service for heroku websockets

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,9 +38,10 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true,
-                'host' => '127.0.0.1',
-                'port' => 6001,
-                'scheme' => 'http',
+                // Local websocket server config -----
+                // 'host' => '127.0.0.1',
+                // 'port' => 6001,
+                // 'scheme' => 'http',
             ],
         ],
 
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -24,10 +24,12 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    forceTLS: false,
-    wsHost: window.location.hostname,
-    wsPort: 6001,
-    wssPort: 6001,
-    disableStats: true,
-    enabledTransports: ['ws'],
+    encrypted: true,
+    // Local websocket server config -----
+    // forceTLS: false,
+    // wsHost: window.location.hostname,
+    // wsPort: 6001,
+    // wssPort: 6001,
+    // disableStats: true,
+    // enabledTransports: ['ws'],
 });
