ecbc8e1bb81badc6c15d3bf0123bf01282d552b0
Josh Codd <jjc21@live.co.uk>
2021-12-28 16:56:30 +0000

    Test

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -91,14 +91,16 @@ class PostController extends Controller
     public function uploadImage(Request $request)
     {
         if ($request->hasFile('image')) {
+            dd("test 1");
             $file = $request->file('image');
-
+            dd("test 2");
             $resized = Image::make($file)->resize(600, null, function ($constraint) {
                 $constraint->aspectRatio();
             });
             // ->encode('webp', 90);
-
+            dd("test 3");
             $original_name = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+            dd("test 4");
             $file_path = "images/" . md5(time()) . '_' . $original_name . ".jpg";
 
             dd($original_name, $file_path);
